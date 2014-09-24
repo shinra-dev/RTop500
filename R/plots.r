a3c370fcf224be516fd4a9c66393893cd8dac760
Drew Schmidt <schmidt@math.utk.edu>
2014-09-23 20:52:30 -0400

    added value label to plotter

--- R/plots.r
+++ R/plots.r
@@ -1,4 +1,4 @@
-top500_plotter <- function(df, my.val, lab)
+top500_plotter <- function(df, my.val, lab, my.val.lab)
 {
   g <- ggplot(df, aes(date, value)) + 
          geom_line(aes(colour=Type)) + 
@@ -11,7 +11,8 @@ top500_plotter <- function(df, my.val, lab)
     labeldata <- data.frame(date=df[nrow(df), 1L], value=my.val)
     
     g <- g + geom_hline(yintercept=my.val, linetype="dashed") + 
-             geom_text(aes(vjust=-.5, hjust=.8), label="My Computer", data=labeldata)
+             geom_text(aes(vjust=-.5, hjust=.8), label="My Computer", data=labeldata) + 
+             geom_text(aes(vjust=1.5, hjust=.8), label=paste(round(my.val, 2), my.val.lab), data=labeldata)
   }
   
   g <- g + scale_y_log10(breaks=trans_breaks("log10", function(x) 10^x),
@@ -34,17 +35,27 @@ plot_top500 <- function(columns=c("Min", "Mean", "Max"), type="theoretical", my.
   {
     top <- top500.diff()
     lab <- "Difference Between Theoretical Peak and Performance on Linpack Benchmark"
+    my.val.lab <- ""
   }
   else
   {
     top <- top500.summary(type=type)
     
     if (type == "theoretical")
+    {
       lab <- "Theoretical Peak GFLOPs"
+      my.val.lab <- "Peak GFLOPs   "
+    }
     else if (type == "linpack")
+    {
       lab <- "GFLOPs Performance on the Linpack Benchmark"
+      my.val.lab <- "Linpack GFLOPs    "
+    }
     else if (type == "ncores")
+    {
       lab <- "Number of Cores"
+      my.val.lab <- "Cores"
+    }
   }
   
   top <- top[, c("date", columns)]
@@ -52,7 +63,11 @@ plot_top500 <- function(columns=c("Min", "Mean", "Max"), type="theoretical", my.
   df <- tidyr::gather(top, Type, value, -date)
   df$Type <- factor(df$Type, levels=rev(levels(df$Type)))
   
-  g <- top500_plotter(df=df, my.val=my.val, lab=lab)
+  if (is.null(my.val))
+    my.val.lab <- ""
+  
+  g <- top500_plotter(df=df, my.val=my.val, lab=lab, my.val.lab=my.val.lab)
   
   return( g )
 }
+
