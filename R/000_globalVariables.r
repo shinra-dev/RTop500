803efc80e9cc7d15bb908e9917319cd4f641ec18
wrathematics <wrathematics@gmail.com>
2014-10-06 20:42:34 -0400

    added declaration of global variables

---
+++ R/000_globalVariables.r
@@ -0,0 +1,22 @@
+### Do not delete this file and file name!!
+### This file should be loaded before all other *.r files.
+
+### This is to avoid the fale positive messages from R CMD check.
+###   "no visible binding for global variable"
+### Suggested by Prof Brian Ripley
+### ?globalVariables
+
+utils::globalVariables( c(".__TOP500_datafiles",
+  "TOP500_199306", "TOP500_199311", "TOP500_199406", "TOP500_199411",
+  "TOP500_199506", "TOP500_199511", "TOP500_199606", "TOP500_199611",
+  "TOP500_199706", "TOP500_199711", "TOP500_199806", "TOP500_199811",
+  "TOP500_199906", "TOP500_199911", "TOP500_200006", "TOP500_200011",
+  "TOP500_200106", "TOP500_200111", "TOP500_200206", "TOP500_200211",
+  "TOP500_200306", "TOP500_200311", "TOP500_200406", "TOP500_200411",
+  "TOP500_200506", "TOP500_200511", "TOP500_200606", "TOP500_200611",
+  "TOP500_200706", "TOP500_200711", "TOP500_200806", "TOP500_200811",
+  "TOP500_200906", "TOP500_200911", "TOP500_201006", "TOP500_201011",
+  "TOP500_201106", "TOP500_201206", "TOP500_201211", "TOP500_201306",
+  "TOP500_201311", "TOP500_201406")
+)
+
